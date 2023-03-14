138ae4ee39b2491ec5663af085bc1ea52d3f6dd0
一刀 <yhonglai@163.com>
2023-03-14 23:58:45 +0800

    refactor 更新winui 3 demo

---
+++ src/DaoLang.Base/ObservableString.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace DaoLang.Base
+{
+    public class ObservableString : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableString(string value)
+        {
+            this.Value = value;
+        }
+
+        private string _value;
+        public string Value
+        {
+            get => this._value;
+            set
+            {
+                if (this._value == value)
+                {
+                    return;
+                }
+                this._value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+    }
+}
--- src/DaoLang.WinUI3.Sample/App.xaml.cs
+++ src/DaoLang.WinUI3.Sample/App.xaml.cs
@@ -1,23 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using DaoLang.Base;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -53,12 +38,19 @@ namespace DaoLang.WinUI3.Sample
 
         private static void LanguageDemo_LanguageChanged(LanguageEventArgs args)
         {
-            // 替换语言资源
-            //Application.Current.Resources.MergedDictionaries.Clear();
-            //Application.Current.Resources.MergedDictionaries.Add(args.ResourceDictionary);
-
-            //Application.Current.Resources.ThemeDictionaries.Clear();
-            Application.Current.Resources["ControlsName"] = "babi";
+            foreach (var item in args.ResourceDictionary)
+            {
+                var key = item.Key as string;
+                var value = item.Value as string;
+                if (Application.Current.Resources.ContainsKey(key))
+                {
+                    (Application.Current.Resources[key] as ObservableString).Value = value;
+                }
+                else
+                {
+                    Application.Current.Resources.Add(key, new ObservableString(value));
+                }
+            }
         }
 
         private Window m_window;
